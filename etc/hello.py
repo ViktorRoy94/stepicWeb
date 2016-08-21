f3672a49df307eee9b0d79750e89e8790f70412c
viktor <viktor.roy94@mail.ru>
2016-08-21 17:35:40 +0000

    wsgi 1.2

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,3 @@
 bind = '0.0.0.0:8080'
-accesslog ='/home/box/web/log/gunicorn_access.log' 
-errorlog ='/home/box/web/log/gunicorn_error.log' 
-pythonpath ='/usr/local/bin/python2.7'
+pythonpath ='/home/box/web'
+workers = 4
