68d80800dc24e0542807843ab29b6e2b4445fd8a
viktor <viktro.roy94@mail.ru>
2016-08-21 17:52:06 +0000

    wsgi complete

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c hello.py hello:app&
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app&
 
